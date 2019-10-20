bb0547792fab7cb55c4ecabe258981b668d53c7d
Joonas Kaski <joonas.kaski@gmail.com>
2019-10-20 21:05:22 +0300

    Rewrite form and formElement with react-final-form

--- src/components/entryForm/entryForm.js
+++ src/components/entryForm/entryForm.js
@@ -1,16 +1,82 @@
 import React from 'react';
 import classes from './entryForm.module.css';
 import FormElement from './formElement/formElement';
+import {Form, Field} from 'react-final-form';
 
 const EntryForm = () => {
-    return (
-        <div className={classes.main}>
-            <h2>New Entry</h2>
-            <FormElement type="range" name="happiness" text="Overall happiness"/>
-            <FormElement type="range" name="sleep" text="Sleep" />
-            <FormElement type="checkbox" name="meditation" text="Did you meditate today?"/>
-        </div>
-    )
-}
+  const oldForm = (
+    <div className={classes.main}>
+      <h2>New Entry</h2>
+      <FormElement type="range" name="happiness" text="Overall happiness" />
+      <FormElement type="range" name="sleep" text="Sleep" />
+      <FormElement
+        type="checkbox"
+        name="meditation"
+        text="Did you meditate today?"
+      />
+    </div>
+  );
+
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+  const onSubmit = async values => {
+    await sleep(300);
+    window.alert(JSON.stringify(values, 0, 2));
+  };
+
+  const newForm = (
+    <>
+      <div className={classes.main}>
+        <h2>New Entry</h2>
+        <Form
+          onSubmit={onSubmit}
+          initialValues={{happiness: 5, sleep: 5, meditation: false}}
+          render={({handleSubmit, form, submitting, pristine, values}) => (
+            <form onSubmit={handleSubmit}>
+              <FormElement
+                type="checkbox"
+                name="meditation"
+                text="Meditation"
+              />
+
+              <FormElement
+                type="range"
+                name="happiness"
+                text="Overall happiness"
+              />
+
+              <FormElement
+                type="range"
+                name="sleep"
+                text="Sleep quality"
+              />
+
+              <FormElement type="notes" placeholder="Notes" />
+
+              <div className={classes.buttons}>
+                <button type="submit" disabled={submitting || pristine}>
+                  Submit
+                </button>
+
+                <button
+                  type="button"
+                  onClick={form.reset}
+                  disabled={submitting || pristine}
+                >
+                  Reset
+                </button>
+              </div>
+              <div className={classes.preview}>
+                <pre>{JSON.stringify(values, 0, 2)}</pre>
+              </div>
+            </form>
+          )}
+        />
+      </div>
+    </>
+  );
+
+  return newForm;
+};
 
 export default EntryForm;
--- src/components/entryForm/formElement/formElement.js
+++ src/components/entryForm/formElement/formElement.js
@@ -1,46 +1,60 @@
 import React from 'react';
 import classes from './formElement.module.css';
+import {Field} from 'react-final-form';
 
 const FormElement = props => {
-    
-    const chooseElement = (props) => {
-        switch(props.type){
-            case "checkbox": return checkbox;
-            case "range": return range;
+  const chooseElement = props => {
+    switch (props.type) {
+      case 'checkbox':
+        return checkbox;
+      case 'range':
+        return range;
+      case 'notes':
+        return notes;
 
-            default: return;
-        }
+      default:
+        return;
     }
+  };
 
-    const checkbox = (
+  const notes = (
+    <div className={classes.notes}>
+      <label for="notes">Notes</label>
+      <div className={classes.noteboxContainer}>
+      <Field className={classes.notebox} id="notes" name="notes" component="textarea" placeholder={props.placeholder} />
+    </div>
+    </div>
+  );
+
+  const checkbox = (
     <div className={classes.input}>
       <label for={props.name}>{props.text}</label>
-      <input
-        type="checkbox"
-        min="1"
-        max="10"
-        step="0.5"
-        defaultValue="5"
+      <Field
         name={props.name}
-      ></input>
+        id={props.name}
+        component="input"
+        type="checkbox"
+      />
     </div>
   );
 
   const range = (
     <div className={classes.input}>
       <label for={props.name}>{props.text}</label>
-      <input
+      <Field
+        name={props.name}
+        id={props.name}
+        component="input"
         type="range"
         min="1"
         max="10"
         step="0.5"
         defaultValue="5"
-        name={props.name}
-      ></input>
+      />
     </div>
   );
 
-  return <div className={classes.main}>{chooseElement(props)}</div>;
+  return chooseElement(props);
 };
 
 export default FormElement;
